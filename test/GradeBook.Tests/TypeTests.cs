be4fa235e2c621a329dd71abc2d14fdf2ae6e5ea
fre5 <enjsof@gmail.com>
2021-11-14 19:16:00 -0800

    Unit tests

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -32,23 +32,23 @@ namespace GradeBook.Tests
             return message;
         }
 
-        // [Fact]
-        // public void GetBookReturnsDifferentObjects()
-        // {
-        //     var book1 = GetBook("Book 1");
-        //     SetName(book1, "New Name");
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
 
-        //     Assert.Equal("", book1.Name);
-        // }
+            Assert.Equal("New Name", book1.Name);
+        }
 
-        // private void SetName(Book book, string name)
-        // {
-        //     book.Name = name;
-        // }
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
 
-        // Book GetBook(string name)
-        // {
-        //     return new Book(name);
-        // }
+        Book GetBook(string name)
+        {
+            return new InMemoryBook(name);
+        }
     }
 }
